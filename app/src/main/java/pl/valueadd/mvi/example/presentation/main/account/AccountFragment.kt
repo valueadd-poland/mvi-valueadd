a8d9f4d010b149f04df6a5f2389c575bf0433fdd
Arkadiusz Pałka <arkadiusz.palka@valueadd.pl>
2020-02-07 08:51:18 +0100

    Add DestroyViewIntent delegate implementation
    Add documentation at DestroyViewIntentDelegate

--- app/src/main/java/pl/valueadd/mvi/example/presentation/main/account/AccountFragment.kt
+++ app/src/main/java/pl/valueadd/mvi/example/presentation/main/account/AccountFragment.kt
@@ -1,14 +1,13 @@
 package pl.valueadd.mvi.example.presentation.main.account
 
 import io.reactivex.Observable
-import io.reactivex.subjects.PublishSubject
-import io.reactivex.subjects.Subject
 import kotlinx.android.synthetic.main.fragment_account.emailText
 import kotlinx.android.synthetic.main.fragment_account.firstNameText
 import kotlinx.android.synthetic.main.fragment_account.surnameText
 import pl.valueadd.mvi.example.R
 import pl.valueadd.mvi.example.presentation.base.AbstractBackMviFragment
 import pl.valueadd.mvi.example.utility.extension.applyTextChanges
+import pl.valueadd.mvi.fragment.delegate.destroyview.DestroyViewIntentDelegate
 import javax.inject.Inject
 
 class AccountFragment :
@@ -27,9 +26,8 @@ class AccountFragment :
     override val titleRes: Int =
         R.string.account_title
 
-
-    private val destroyViewIntent: Subject<AccountView.Intent>
-        by lazy { PublishSubject.create<AccountView.Intent>() }
+    private val destroyViewIntent
+        by DestroyViewIntentDelegate(this, ::provideDestroyViewIntent)
 
     override fun render(state: AccountViewState) {
         firstNameText.applyTextChanges(state.firstName)
@@ -41,13 +39,10 @@ class AccountFragment :
         destroyViewIntent
     )
 
-    override fun onStop() {
-        destroyViewIntent.onNext(AccountView.Intent.OnDestroyView(
+    private fun provideDestroyViewIntent(): AccountView.Intent =
+        AccountView.Intent.OnDestroyView(
             firstNameText.text.toString(),
             surnameText.text.toString(),
             emailText.text.toString()
-        ))
-        super.onStop()
-    }
-
+        )
 }
